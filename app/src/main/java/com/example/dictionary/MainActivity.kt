57a72d243decbe69c21121e89c692cc7613c21ab
wakelift <admin@mbp-wakelift.local>
2021-12-12 05:22:56 +0300

    init: dependencies + package structure

---
+++ app/src/main/java/com/example/dictionary/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.dictionary
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.dictionary.ui.theme.DictionaryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DictionaryTheme {
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DictionaryTheme {
+
+    }
+}
---
+++ app/src/main/java/com/example/dictionary/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.example.dictionary.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val DeepDive300 = Color(0xFF8DA2AF)
+val DeepDive700 = Color(0xFF425865)
+val Raspberry_50 = Color(0xFFFAE4ED)
+val Raspberry500 = Color(0xFFE3256B)
